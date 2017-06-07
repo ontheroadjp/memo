edb7b2ae8566012beab58310f1e801c11fb3dced
@ontheroad_jp <dev@ontheroad.jp>
2017-06-07 23:51:43 +0900

    [Refactor] decorate echo message

--- bin/publish_to_githubpages.sh
+++ bin/publish_to_githubpages.sh
@@ -10,25 +10,25 @@ then
     exit 1;
 fi
 
-echo "Deleting old publication"
+echo ">> Deleting old publication"
 rm -rf public
 mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
+echo ">> Checking out gh-pages branch into public"
 git worktree add -B gh-pages public origin/gh-pages
 
-echo "Removing existing files"
+echo ">> Removing existing files"
 rm -rf public/*
 
-echo "Generating site"
+echo ">> Generating site"
 hugo || {
     echo "failed: generage static files."
     exit 1;
 }
 
-echo "Updating gh-pages branch"
+echo ">> Updating gh-pages branch"
 cd public
 git add --all
 git commit -m "Publishing to gh-pages (publish.sh)"
