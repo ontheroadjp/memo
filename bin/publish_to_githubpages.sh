3f73da40a08f69bd2f0cc7829f8e736eb6867320
@ontheroad_jp <dev@ontheroad.jp>
2017-06-07 23:38:58 +0900

    [Modify] minor modify

--- bin/publish_to_githubpages.sh
+++ bin/publish_to_githubpages.sh
@@ -23,7 +23,12 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
-
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+hugo && {
+    echo "Updating gh-pages branch"
+    cd public 
+    && git add --all 
+    && git commit -m "Publishing to gh-pages (publish.sh)"
+    && echo "success!"
+} || {
+    echo "failed: generage static files.
+}
